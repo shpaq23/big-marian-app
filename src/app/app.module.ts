fe37f85b9c5eec67225c85e5f287679d340d915b
Michal Szpak <michal.szpak@softwareplant.com>
2022-01-10 17:13:32 +0100

    some changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BigMarianGameModule } from 'big-marian';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+	  BrowserModule,
+	  BigMarianGameModule
   ],
   providers: [],
   bootstrap: [AppComponent]
